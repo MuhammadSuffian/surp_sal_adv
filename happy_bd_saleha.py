5f9ac43abdb40402ad273b6064755c80c7fc8020
Muhammad Suffian Tafoor <112380672+MuhammadSuffian@users.noreply.github.com>
2025-11-18 22:15:53 +0500

    Update happy_bd_saleha.py

--- happy_bd_saleha.py
+++ happy_bd_saleha.py
@@ -465,7 +465,8 @@ def is_birthday():
     today = datetime.now(timezone).date()
     # You can change this to Waris's actual birthday date
     # For example, if Waris's birthday is August 10th, change it to: return today.month == 8 and today.day == 10
-    return today.month == 9 and today.day == 20  # Saliha's birthday is December 27th
+    return today.month == 12 and today.day == 27
+      # Saliha's birthday is December 27th
 
 # Calculate time until Saliha's birthday
 def time_until_birthday():
